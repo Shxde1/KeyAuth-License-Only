9cbc04dfd60b9cce5951ffe7b37fc86e63e6cb83
Shxde <112267394+Shxde1@users.noreply.github.com>
2022-09-01 23:32:17 -0400

    Add files via upload
    C++

---
+++ KeyAuth License Only Example/main.cpp
@@ -0,0 +1,77 @@
+#include <Windows.h>
+#include "auth.hpp"
+#include <string>
+#include "skStr.h"
+std::string tm_to_readable_time(tm ctx);
+static std::time_t string_to_timet(std::string timestamp);
+static std::tm timet_to_tm(time_t timestamp);
+
+using namespace KeyAuth;
+
+std::string name = ""; // application name. right above the blurred text aka the secret on the licenses tab among other tabs
+std::string ownerid = ""; // ownerid, found in account settings. click your profile picture on top right of dashboard and then account settings.
+std::string secret = ""; // app secret, the blurred text on licenses tab and other tabs
+std::string version = "1.0"; // leave alone unless you've changed version on website
+std::string url = "https://keyauth.win/api/1.1/"; // change if you're self-hosting
+std::string sslPin = "ssl pin key (optional)"; // don't change unless you intend to pin public certificate key. you can get here in the "Pin SHA256" field https://www.ssllabs.com/ssltest/analyze.html?d=keyauth.win&latest. If you do this you need to be aware of when SSL key expires so you can update it
+
+/*
+  try to find something to encrypt these even if you might encrypt after compilation time just encrypt them using xor even thought it can easily be decrypted
+*/
+
+api KeyAuthApp(name, ownerid, secret, version, url, sslPin);
+
+int main()
+{
+	SetConsoleTitleA(skCrypt("Loader"));
+	std::cout << skCrypt("\n\n Connecting..");
+	KeyAuthApp.init();
+	if (!KeyAuthApp.data.success)
+	{
+		std::cout << skCrypt("\n Status: ") << KeyAuthApp.data.message;
+		Sleep(1500);
+		exit(0);
+	}
+
+
+
+	std::string key;
+
+	std::cout << skCrypt("\n Enter license: ");
+	std::cin >> key;
+	KeyAuthApp.license(key);
+
+	if (!KeyAuthApp.data.success)
+	{
+		std::cout << skCrypt("\n Status: ") << KeyAuthApp.data.message;
+		Sleep(1500);
+		exit(0);
+	}
+
+	std::cout << skCrypt("Add Your Loader Stuff Here");
+	
+
+	return 0;
+}
+
+std::string tm_to_readable_time(tm ctx) {
+	char buffer[80];
+
+	strftime(buffer, sizeof(buffer), "%a %m/%d/%y %H:%M:%S %Z", &ctx);
+
+	return std::string(buffer);
+}
+
+static std::time_t string_to_timet(std::string timestamp) {
+	auto cv = strtol(timestamp.c_str(), NULL, 10); // long
+
+	return (time_t)cv;
+}
+
+static std::tm timet_to_tm(time_t timestamp) {
+	std::tm context;
+
+	localtime_s(&context, &timestamp);
+
+	return context;
+}
